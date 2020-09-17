596a502f32bd05ca693bc37d45dccc4b26111d01
97arkham <97arkham>
2020-09-17 16:01:39 +0700

    hahaha

--- src/views/perusahaan/perusahaan.vue
+++ src/views/perusahaan/perusahaan.vue
@@ -1,16 +1,38 @@
 <template>
   <div>
       <navbarPerusahaan/>
-      <h1>halaman utama perusahaan</h1>
+        <v-carousel class="mb-10" cycle height="400" hide-delimiter-background show-arrows-on-hover>
+      <v-carousel-item v-for="(item,i) in items" :key="i" :src="item.src"></v-carousel-item>
+    </v-carousel>
+      <branda/>
   </div>
 </template>
 
 <script>
 import navbarPerusahaan from "@/components/navbar-perusahaan"
-
+import j1 from "@/assets/jombroton/jombroton1.jpg";
+import j2 from "@/assets/jombroton/jombroton2.jpg";
+import j3 from "@/assets/jombroton/jombroton3.jpg";
+import branda from "@/components/pelamar/kolom.vue";
 export default {
+     data() {
+    return {
+      items: [
+        {
+          src: j1
+        },
+        {
+          src: j2
+        },
+        {
+          src: j3
+        }
+      ]
+    };
+  },
     components: {
-        navbarPerusahaan
+        navbarPerusahaan,
+        branda
     }
 }
 </script>
